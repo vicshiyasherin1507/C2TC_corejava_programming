0067fe9cdad8e41f0710edfc2001f4c701d11b36
WELCOME <WELCOME@DESKTOP-L7JP3CA>
2025-10-26 17:34:22 +0530

    Day nineteen of core java

---
+++ core_java_courses/src/com/tnsif/daynineteen/V1/Message.java
@@ -0,0 +1,6 @@
+package com.tnsif.daynineteen;
+
+@FunctionalInterface
+public interface Message{  
+    public void greet(String name);    
+ } 
---
+++ core_java_courses/src/com/tnsif/daynineteen/V1/Palindrome.java
@@ -0,0 +1,9 @@
+package com.tnsif.daynineteen.V1;
+
+public class Palindrome<T> {
+
+	public Palindrome() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
